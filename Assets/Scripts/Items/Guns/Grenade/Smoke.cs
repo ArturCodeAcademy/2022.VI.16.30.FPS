67675f29495fc2288542a2bfa690e4156c99cb64
ArturCodeAcademy <123232198+ArturCodeAcademy@users.noreply.github.com>
2023-05-20 17:53:39 +0300

    3_8 pamoka

--- Assets/Scripts/Items/Guns/Grenade/Smoke.cs
+++ Assets/Scripts/Items/Guns/Grenade/Smoke.cs
@@ -5,10 +5,14 @@ public class Smoke : Grenade
     [SerializeField, Min(0)] private float _exploadPause;
     
     private ParticleSystem _particleSystem;
+    private AudioSource _audioSourceExplosion;
+    private AudioSource _audioSourceSmoke;
 
     private void Start()
     {
         _particleSystem = GetComponent<ParticleSystem>();
+		_audioSourceExplosion = GetComponent<AudioSource>();
+		_audioSourceSmoke = transform.GetChild(0).GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -18,7 +22,11 @@ public class Smoke : Grenade
 
             _exploadPause -= Time.deltaTime;
         if (_exploadPause <= 0)
+        {
             _particleSystem.Play();
+			_audioSourceExplosion.Play();
+			_audioSourceSmoke.Play();
+		}
     }
 
     private void OnParticleSystemStopped()
